10da275cbbc49253de0e050d1d40a6fcdbdadba1
slowpon <slowpon@outlook.com>
2021-11-28 01:59:00 -0500

    part of frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Drizzle } from '@drizzle/store';
+import { drizzleReactHooks } from "@drizzle/react-plugin";
+
+import drizzleOptions from "./drizzleOptions";
+import LoadingContainer from './LoadingContainer.js';
+import ICOInfo from './ICOInfo.js';
+import Investor from './Investor.js';
+import Admin from './Admin.js';
+
+const drizzle = new Drizzle(drizzleOptions);
+const { DrizzleProvider } = drizzleReactHooks;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>ICO</h1>
+        <DrizzleProvider drizzle={drizzle}>
+          <LoadingContainer>
+            <ICOInfo />
+            <Investor />
+            <Admin />
+          </LoadingContainer>
+        </DrizzleProvider>
+      </div>
   );
 }
 
